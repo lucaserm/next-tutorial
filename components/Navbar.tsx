a8654c7eee1ad2e0070a684e84f4721f618c2f5b
Lucas Macedo <lucasedumacedo@gmail.com>
2024-05-24 22:53:43 -0400

    Criando uma página de contador para mostrar que o Next também serve como client side

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -5,6 +5,7 @@ function Navbar() {
   return (
     <nav className='max-w-3xl mx-auto py-4 flex gap-x-4'>
       <Link href='/'>Home</Link>
+      <Link href='/counter'>Counter</Link>
       <Link href='/about'>About</Link>
       <Link href='/info/contact'>Contact</Link>
     </nav>
