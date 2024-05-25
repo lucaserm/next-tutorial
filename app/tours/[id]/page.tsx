659cf81fc17cafa8601659cd6962c25a7ea441e7
Lucas Macedo <lucasedumacedo@gmail.com>
2024-05-25 10:24:52 -0400

    Configurando imagens remotas e local

--- app/tours/[id]/page.tsx
+++ app/tours/[id]/page.tsx
@@ -1,12 +1,40 @@
 import React from 'react'
+import mapsImg from '@/images/maps.jpg';
+import Image from 'next/image'
+const url = 'https://www.course-api.com/images/tours/tour-1.jpeg';
 
 function Page({ params }: { params: { id: string } }) {
-
   return (
     <div>
       <h1 className="text-4xl">ID : {params.id}</h1>
+      <section className="flex gap-x-4 mt-4">
+        {/* Local image */}
+        <div>
+          <Image
+            src={mapsImg}
+            alt='maps'
+            priority
+            width={192}
+            height={192}
+            className='w-48 h-48 object-cover rounded'
+          />
+          <h2>Local image</h2>
+        </div>
+        {/* Remote image */}
+        <div>
+          <Image
+            src={url}
+            alt='tour'
+            width={192}
+            height={192}
+            priority
+            className='w-48 h-48 object-cover rounded'
+          />
+          <h2>Remote image</h2>
+        </div>
+      </section>
     </div>
   )
 }
 
-export default Page
+export default Page;
