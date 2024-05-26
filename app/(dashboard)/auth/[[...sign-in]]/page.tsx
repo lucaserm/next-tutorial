19a04ae9308e0fb4f65853973042077a51e09838
Lucas Macedo <lucasedumacedo@gmail.com>
2024-05-26 00:37:28 -0400

    Opções de rotas

---
+++ app/(dashboard)/auth/[[...sign-in]]/page.tsx
@@ -0,0 +1,10 @@
+import React from 'react'
+
+function SignInPage({ params }: { params: { 'sign-in': string[] } }) {
+  console.log(params)
+  return (
+    <div>SignInPage: {params['sign-in'][1]}</div>
+  )
+}
+
+export default SignInPage
